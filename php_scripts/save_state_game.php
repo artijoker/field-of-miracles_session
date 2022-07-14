6a4193a958427cb6120c6a5c3e9338e18e1a8a72
artijoker <59118504+artijoker@users.noreply.github.com>
2022-07-14 17:02:43 +0300

    Update save_state_game.php

--- php_scripts/save_state_game.php
+++ php_scripts/save_state_game.php
@@ -2,21 +2,19 @@
 require("./keys.php");
 
 session_start();
-
+setcookie(
+       session_name(),
+       session_id(),
+       [
+           'path' => '/',
+           'secure' => true,
+           'samesite' => 'None',
+       ]
+   );
 $data = json_decode(file_get_contents("php://input"), true);
 
 if (count(array_diff(KEYS, array_keys($data))) === 0) {
     foreach (KEYS as $key) {
         $_SESSION[$key] = $data[$key];
     }
-
-    setcookie(
-        session_name(),
-        session_id(),
-        [
-            'path' => '/',
-            'secure' => true,
-            'samesite' => 'None',
-        ]
-    );
 }
